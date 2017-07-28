90f3bd3b7d336a8ead8f38aa3cb415f1cc369e99
Sergey <sunliveua@gmail.com>
2017-07-28 21:31:16 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
 @echo on
-npm i --save-dev gulp gulp-stylus gulp-plumber gulp-group-css-media-queries gulp-clean-css gulp-autoprefixer browser-sync gulp-rename gulp-uglifyjs smart-grid
+npm i --save-dev gulp gulp-stylus gulp-plumber gulp-group-css-media-queries gulp-clean-css gulp-autoprefixer browser-sync gulp-rename gulp-uglify smart-grid
 pause
