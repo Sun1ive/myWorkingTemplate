5fd87e6d3d7aec1066fcde312293d5294bdeb90b
Sergey <sunliveua@gmail.com>
2017-08-02 10:28:54 +0300

    0.5
    +babel

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
-@echo on
-npm i --save-dev gulp gulp-stylus gulp-plumber gulp-group-css-media-queries gulp-clean-css gulp-autoprefixer browser-sync gulp-rename gulp-uglify smart-grid
+@echo off
+npm i --save-dev gulp gulp-stylus gulp-plumber gulp-group-css-media-queries gulp-clean-css gulp-autoprefixer browser-sync gulp-rename gulp-uglify smart-grid gulp-babel babel-preset-es2015
 pause
