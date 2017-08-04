145b94d697b478b0ba7f9c4a91ca7342d4e53ee2
Sergey <sunliveua@gmail.com>
2017-08-04 18:06:27 +0300

    Delete install.bat

--- install.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-npm i --save-dev gulp gulp-stylus gulp-plumber gulp-group-css-media-queries gulp-clean-css gulp-autoprefixer browser-sync gulp-rename gulp-uglify smart-grid gulp-babel babel-preset-es2015
-pause
